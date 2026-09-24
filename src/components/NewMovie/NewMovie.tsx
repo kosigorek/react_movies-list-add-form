84f9899358c7612ee7e64e6c4dba5cf4e1a591a0
Pianist <kosigorek@gmail.com>
2026-09-24 15:19:16 +0300

    fixed issue

--- src/components/NewMovie/NewMovie.tsx
+++ src/components/NewMovie/NewMovie.tsx
@@ -15,6 +15,7 @@ const initialFormValues: Movie = {
 };
 
 const urlPattern =
+  // eslint-disable-next-line max-len
   /^((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=+$,\w]+@)?[A-Za-z0-9.-]+|(?:www\.|[-;:&=+$,\w]+@)[A-Za-z0-9.-]+)((?:\/[+~%/.\w-_]*)?\??(?:[-+=&;%@,.\w_]*)#?(?:[,.!/\\\w]*))?)$/;
 
 function validateUrl(value: string) {
